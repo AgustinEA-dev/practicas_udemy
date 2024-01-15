161787080f160499339ca0d8dc8146f6a88c6c26
unknown <ag_antro@hotmail.com>
2024-01-15 08:53:47 -0300

    ultimo commit

--- ejs/index.js
+++ ejs/index.js
@@ -1,12 +1,35 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.set('')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
-    res.send('HI!!')
+    res.render('home.ejs')
 })
 
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1
+    res.render('random', { num: num })
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params
+    res.render('subreddit', { subreddit })
+})
+
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Jaime', 'Bruno', 'Simón'
+    ]
+    res.render('cats', { cats })
+})
+
+
+
+
+//)))))))))) -LISTEN- ((((((((((
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 })
