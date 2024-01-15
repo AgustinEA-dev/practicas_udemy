df4c8c3248ce0b17cb946edfee3addab733698f2
unknown <ag_antro@hotmail.com>
2024-01-15 07:35:22 -0300

    ultimo commit

---
+++ ejs/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.set('')
+
+app.get('/', (req, res) => {
+    res.send('HI!!')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
