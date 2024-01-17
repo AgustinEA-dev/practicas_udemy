c977e64bc88f85b594f8f59b189205b516e154b7
unknown <ag_antro@hotmail.com>
2024-01-17 10:39:48 -0300

    último commit

---
+++ ejs/restDemo/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const { v4: uuid } = require('uuid');
+uuid()
+
+app.use(express.urlencoded({ extended: true })) //se usa para realizar un parseIn del req.body
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+const comments = [
+    {
+        id: uuid(),
+        username: 'Agu',
+        comment: 'lol that is so funny!'
+    },
+    {
+        id: uuid(),
+        username: 'Sara',
+        comment: 'Not funny'
+    },
+    {
+        id: uuid(),
+        username: 'Pedro',
+        comment: 'Please make a tutorial about carne asada.'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
+
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new')
+})
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body
+    comments.push({ username, comment, id: uuid() })
+    res.redirect('/comments')
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/show', { comment })
+})
+
+
+app.get('/tacos', (req, res) => {
+    res.send('Tacos response')
+})
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body
+    console.log(req.body)
+
+})
+
+app.listen(3000, () => {
+    console.log('ON PORT 3000')
+}) 
